45ca8ed1779bb1667cf0e3d464e319ae33b3da47
charliebarber <charlieb@protonmail.com>
2024-03-05 14:49:25 +0000

    Clean traces folder on run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Simulating ns3 on file: $1"
 
+rm -rf traces/
+mkdir traces
 cp user_src/* scratch/
 
 export NS_LOG=FRRQueue=level_all
 
-./ns3 build 
-./ns3 run "scratch/$1" > traces/output.log 2>&1
-
+./ns3 build
+./ns3 run "scratch/$1" >traces/output.log 2>&1
