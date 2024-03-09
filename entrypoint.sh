ece150ade1e40f738eb76331de13750242be3275
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-03-09 21:14:58 +0000

    :sparkles: if no argument, don't execute scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,26 +14,12 @@ if [ $? -ne 0 ]; then
 fi
 
 if [ -z "$1" ]; then
-
-  DIRECTORY="scratch"
-
-  for fullpath in "$DIRECTORY"/*.cc
-  do
-    filename=$(basename -- "$fullpath")
-    name="${filename%.*}"
-    ./ns3 run "$name"
-
-    if [ $? -ne 0 ]; then
-      echo "multiple ./ns3 run failed at $name"
-      exit 1
-    fi
-  done
   exit 0
 fi
 
 ./ns3 run "scratch/$1"
 if [ $? -ne 0 ]; then
-  echo "single ./ns3 run for $name failed"
+  echo "./ns3 run for $name failed"
   exit 1
 fi
 exit 0
