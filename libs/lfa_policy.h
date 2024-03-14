b68b109eb52aa3bc750aa6ee814eb54125d7e158
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-03-14 02:43:51 +0000

    :recycle: update policies to match changes

--- libs/lfa_policy.h
+++ libs/lfa_policy.h
@@ -6,15 +6,18 @@
 
 #include "ns3/queue.h"
 #include "ns3/packet.h"
-#include "ns3/point-to-point-net-device.h"
+#include "point_to_point_frr_net_device.h"
 
 namespace ns3
 {
 
+class LFAPolicy;
+
 class LFAPolicy
 {
   private:
-    std::list<ns3::Ptr<ns3::PointToPointNetDevice>> m_alternateTargets;
+    std::list<ns3::Ptr<ns3::PointToPointFRRNetDevice<LFAPolicy>>>
+        m_alternateTargets;
 
   public:
     LFAPolicy() = default;
@@ -22,7 +25,8 @@ class LFAPolicy
     template <typename... DEVICES>
     void addAlternateTargets(DEVICES&&... devices);
 
-    ns3::Ptr<ns3::PointToPointNetDevice> selectAlternativeTarget();
+    bool reroute(Ptr<Packet> packet, const Address& dest,
+                 uint16_t protocolNumber);
 };
 
 template <typename... DEVICES>
@@ -31,12 +35,16 @@ void LFAPolicy::addAlternateTargets(DEVICES&&... devices)
     (m_alternateTargets.push_back(std::forward<DEVICES>(devices)), ...);
 }
 
-ns3::Ptr<ns3::PointToPointNetDevice> LFAPolicy::selectAlternativeTarget()
+bool LFAPolicy::reroute(Ptr<Packet> packet, const Address& dest,
+                        uint16_t protocolNumber)
 {
-    if (m_alternateTargets.empty()) {
-        return nullptr;
+    for (auto& target : m_alternateTargets) {
+        if (!target->isCongested()) {
+            target->Send(packet, dest, protocolNumber);
+            return true;
+        }
     }
-    return m_alternateTargets.front();
+    return false;
 }
 
 } // namespace ns3
