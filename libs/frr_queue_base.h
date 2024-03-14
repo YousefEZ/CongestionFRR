4118192e5ade69b3097f89c402ab47727a625826
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-03-14 02:42:14 +0000

    :sparkles: add queue base to upcast templated queue

---
+++ libs/frr_queue_base.h
@@ -0,0 +1,28 @@
+#ifndef FRR_QUEUE_BASE_H
+#define FRR_QUEUE_BASE_H
+
+#include "ns3/queue.h"
+#include "ns3/packet.h"
+
+namespace ns3
+{
+
+class FRRQueueBase : public Queue<Packet>
+{
+  protected:
+    using Queue<Packet>::GetContainer;
+    using Queue<Packet>::DoEnqueue;
+    using Queue<Packet>::DoDequeue;
+    using Queue<Packet>::DoRemove;
+    using Queue<Packet>::DoPeek;
+
+  public:
+    FRRQueueBase() = default;
+    virtual ~FRRQueueBase() = default;
+
+    virtual bool isCongested() = 0;
+};
+
+} // namespace ns3
+
+#endif
