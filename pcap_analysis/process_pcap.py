9bdb4c73b060c58108173e3c8b07407d01a4d969
Mike <mike.park.20@ucl.ac.uk>
2024-03-19 14:27:48 +0000

    pcap_analysis.py format

--- pcap_analysis/process_pcap.py
+++ pcap_analysis/process_pcap.py
@@ -17,7 +17,7 @@ def get_IP(packets):
     sender_IP = packets[0].getlayer('IP').src
     receiver_IP = packets[0].getlayer('IP').dst
     return sender_IP, receiver_IP
-    
+
 
 def packet_transfer_time(sender_packets, receiver_packets):
     timestamp = []
@@ -26,7 +26,13 @@ def packet_transfer_time(sender_packets, receiver_packets):
 
     for sender_packet in sender_packets:
         for receiver_packet in receiver_packets:
-            if (sender_packet.getlayer('TCP').seq == receiver_packet.getlayer('TCP').seq) and (sender_packet.getlayer('TCP').ack == receiver_packet.getlayer('TCP').ack) and (sender_packet.getlayer('IP').src == receiver_packet.getlayer('IP').src) and (sender_packet.getlayer('IP').dst == receiver_packet.getlayer('IP').dst) and (sender_packet.getlayer('TCP').window == receiver_packet.getlayer('TCP').window) and (sender_packet.getlayer('TCP').options == receiver_packet.getlayer('TCP').options) and (sender_packet.getlayer('TCP').options[0][1] == receiver_packet.getlayer('TCP').options[0][1]): 
+            if (sender_packet.getlayer('TCP').seq == receiver_packet.getlayer('TCP').seq) and (
+                    sender_packet.getlayer('TCP').ack == receiver_packet.getlayer('TCP').ack) and (
+                    sender_packet.getlayer('IP').src == receiver_packet.getlayer('IP').src) and (
+                    sender_packet.getlayer('IP').dst == receiver_packet.getlayer('IP').dst) and (
+                    sender_packet.getlayer('TCP').window == receiver_packet.getlayer('TCP').window) and (
+                    sender_packet.getlayer('TCP').options == receiver_packet.getlayer('TCP').options) and (
+                    sender_packet.getlayer('TCP').options[0][1] == receiver_packet.getlayer('TCP').options[0][1]):
                 if sender_packet.time > receiver_packet.time:
                     completion_time.append(sender_packet.time - receiver_packet.time)
                     stream_direction.append('down')
@@ -38,6 +44,7 @@ def packet_transfer_time(sender_packets, receiver_packets):
 
     return timestamp, completion_time, stream_direction
 
+
 def flow_completion_time(sender_packets):
     time = sender_packets[-1].time - sender_packets[0].time
     return time
@@ -45,11 +52,12 @@ def flow_completion_time(sender_packets):
 
 def save_plot(timestamp, completion_time, stream_direction):
     os.makedirs("/host/plots", exist_ok=True)
-    plt.plot(timestamp, completion_time, label = stream_direction, marker = 'o')
+    plt.plot(timestamp, completion_time, label=stream_direction, marker='o')
     plt.xlabel("time stamp of the sender of the packet")
     plt.ylabel("transfer completion time")
 
     plt.savefig("/host/plots/completion_time-timestamp.png")
+
 
 if __name__ == '__main__':
     sender_packets = read_pcap("-Sender-0.pcap")
@@ -58,10 +66,6 @@ if __name__ == '__main__':
 
     # timestamp, completion_time, stream_direction = packet_transfer_time(sender_packets, receiver_packets)
 
-
-
-
     fc_time = flow_completion_time(sender_packets)
     print(fc_time)
     # save_plot(timestamp, completion_time, stream_direction)
-    
