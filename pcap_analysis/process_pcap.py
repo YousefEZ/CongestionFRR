3c991330e08f71b29a6e4d936ed3a0662448c7b9
Mike <mike.park.20@ucl.ac.uk>
2024-04-03 16:45:01 +0100

    process_pcap now looks for fin ack, and reads all the files in the traces folder

--- pcap_analysis/process_pcap.py
+++ pcap_analysis/process_pcap.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 def read_pcap(filename):
     packets = []
-    data = scapy.utils.PcapReader("traces/" + filename)
+    data = scapy.utils.PcapReader(filename)
     for packet in data:
         packets.append(packet)
-        print(packet.getlayer('TCP').options)
+        # print(packet.getlayer('TCP').options)
     return packets
 
 
@@ -19,35 +19,43 @@ def get_IP(packets):
     return sender_IP, receiver_IP
 
 
-def packet_transfer_time(sender_packets, receiver_packets):
-    timestamp = []
-    completion_time = []
-    stream_direction = []
+# def packet_transfer_time(sender_packets, receiver_packets):
+#     timestamp = []
+#     completion_time = []
+#     stream_direction = []
+#
+#     for sender_packet in sender_packets:
+#         for receiver_packet in receiver_packets:
+#             if (sender_packet.getlayer('TCP').seq == receiver_packet.getlayer('TCP').seq) and (
+#                     sender_packet.getlayer('TCP').ack == receiver_packet.getlayer('TCP').ack) and (
+#                     sender_packet.getlayer('IP').src == receiver_packet.getlayer('IP').src) and (
+#                     sender_packet.getlayer('IP').dst == receiver_packet.getlayer('IP').dst) and (
+#                     sender_packet.getlayer('TCP').window == receiver_packet.getlayer('TCP').window) and (
+#                     sender_packet.getlayer('TCP').options == receiver_packet.getlayer('TCP').options) and (
+#                     sender_packet.getlayer('TCP').options[0][1] == receiver_packet.getlayer('TCP').options[0][1]):
+#                 if sender_packet.time > receiver_packet.time:
+#                     completion_time.append(sender_packet.time - receiver_packet.time)
+#                     stream_direction.append('down')
+#                 else:
+#                     completion_time.append(receiver_packet.time - sender_packet.time)
+#                     stream_direction.append('up')
+#                 timestamp.append(sender_packet.time)
+#                 break
+#
+#     return timestamp, completion_time, stream_direction
 
-    for sender_packet in sender_packets:
-        for receiver_packet in receiver_packets:
-            if (sender_packet.getlayer('TCP').seq == receiver_packet.getlayer('TCP').seq) and (
-                    sender_packet.getlayer('TCP').ack == receiver_packet.getlayer('TCP').ack) and (
-                    sender_packet.getlayer('IP').src == receiver_packet.getlayer('IP').src) and (
-                    sender_packet.getlayer('IP').dst == receiver_packet.getlayer('IP').dst) and (
-                    sender_packet.getlayer('TCP').window == receiver_packet.getlayer('TCP').window) and (
-                    sender_packet.getlayer('TCP').options == receiver_packet.getlayer('TCP').options) and (
-                    sender_packet.getlayer('TCP').options[0][1] == receiver_packet.getlayer('TCP').options[0][1]):
-                if sender_packet.time > receiver_packet.time:
-                    completion_time.append(sender_packet.time - receiver_packet.time)
-                    stream_direction.append('down')
-                else:
-                    completion_time.append(receiver_packet.time - sender_packet.time)
-                    stream_direction.append('up')
-                timestamp.append(sender_packet.time)
-                break
 
-    return timestamp, completion_time, stream_direction
+def flow_completion_time(packets):
+    TCP_FIN = 0x01
+    TCP_ACK = 0x10
+    # the hex code for flags in TCP is as above
 
-
-def flow_completion_time(sender_packets):
-    time = sender_packets[-1].time - sender_packets[0].time
-    return time
+    # print(packets[0][TCP].flags)
+    for pkt in packets:
+        if TCP in pkt:
+            if pkt[TCP].flags & TCP_FIN and pkt[TCP].flags & TCP_ACK:
+                return pkt.time
+    return None
 
 
 def save_plot(timestamp, completion_time, stream_direction):
@@ -60,12 +68,16 @@ def save_plot(timestamp, completion_time, stream_direction):
 
 
 if __name__ == '__main__':
-    senderPackets = read_pcap("-Sender-0.pcap")
-    receiverPackets = read_pcap("-Receiver-0.pcap")
-    senderIP, receiverIP = get_IP(senderPackets)
+    files = os.listdir("traces")
+    print(os.listdir("traces"))
 
-    # timestamp, completion_time, stream_direction = packet_transfer_time(sender_packets, receiver_packets)
+    for file in files:
+        if file.endswith(".pcap"):
+            senderPackets = read_pcap("traces/" + file)
+            senderIP, receiverIP = get_IP(senderPackets)
 
-    fc_time = flow_completion_time(senderPackets)
-    print(fc_time)
-    # save_plot(timestamp, completion_time, stream_direction)
+            # timestamp, completion_time, stream_direction = packet_transfer_time(sender_packets, receiver_packets)
+
+            fc_time = flow_completion_time(senderPackets)
+            print(file, ":", fc_time)
+            # save_plot(timestamp, completion_time, stream_direction)
