5955d33d9a7124b34373afc764492577c76d1bd9
charliebarber <charlieb@protonmail.com>
2024-04-05 15:56:11 +0100

    Able to run tests with shell script entirely now without rebuilding

--- run_ns3.sh
+++ run_ns3.sh
@@ -2,7 +2,25 @@
 
 echo "Running ns3 script"
 
-mkdir -p traces/delay-primary/1ms/20/frr
-./ns3 run 'scratch/combined-frr.cc --delay_primary=1ms --policy_threshold=20 --dir=traces/delay-primary/1ms/20/frr/'
+run_experiment() {
+	local test_variable=$1
+	local test_value=$2
+	local policy_threshold=$3
+	local dir="traces/$test_variable/$test_value/$policy_threshold/frr/"
+	mkdir -p "$dir"
 
+	./ns3 run "scratch/combined-frr.cc --$test_variable=$test_value --policy_threshold=$policy_threshold --dir=$dir"
+}
+
+# Bandwidth primary experiments
+echo "Bandwidth primary_experiments"
+delay_vals=("0ms" "10ms" "20ms" "30ms" "40ms" "60ms" "80ms" "100ms")
+for delay_val in "${delay_vals[@]}"; do
+	echo "Delay value: $delay_val"
+	for policy_number in 20 40 60 80 99; do
+		run_experiment "delay_primary" "$delay_val" "$policy_number" &
+	done
+done
+
+wait
 echo "NS3 script finished"
