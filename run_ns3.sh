01d6e773bc113f91373cf1e4ebfb3328b7e56c93
charliebarber <charlieb@protonmail.com>
2024-04-06 14:30:54 +0100

    Cmdline running experiements with 4 variables for all policy nums.

--- run_ns3.sh
+++ run_ns3.sh
@@ -14,11 +14,11 @@ run_experiment() {
 
 	./ns3 run "scratch/combined-frr.cc --$test_variable=$test_value --policy_threshold=$policy_threshold --dir=$dir/frr/"
 	./ns3 run "scratch/combined-frr-no-udp.cc --$test_variable=$test_value --policy_threshold=$policy_threshold --dir=$dir/frr-no-udp/"
-	./ns3 run "scratch/combined-frr-no-udp.cc --$test_variable=$test_value --policy_threshold=$policy_threshold --dir=$dir/baseline-no-udp/"
-	./ns3 run "scratch/combined-frr-no-udp.cc --$test_variable=$test_value --policy_threshold=$policy_threshold --dir=$dir/baseline-udp/"
+	./ns3 run "scratch/combined-baseline-udp.cc --$test_vaiable=$test_value --policy_threshold=$policy_threshold --dir=$dir/baseline-udp/"
+	./ns3 run "scratch/combined-baseline-no-udp.cc --$test_variable=$test_value --policy_threshold=$policy_threshold --dir=$dir/baseline-no-udp/"
 }
 
-# Bandwidth primary experiments
+# Delay primary experiments
 echo "Delay Primary experiments"
 delay_vals=("0ms" "10ms" "20ms" "30ms" "40ms" "60ms" "80ms" "100ms")
 for delay_val in "${delay_vals[@]}"; do
@@ -28,7 +28,7 @@ for delay_val in "${delay_vals[@]}"; do
 	done
 done
 
-# Bandwidth primary experiments
+# Delay alternate experiments
 echo "Delay Alternate experiments"
 delay_vals=("0ms" "10ms" "20ms" "30ms" "40ms" "60ms" "80ms" "100ms")
 for delay_val in "${delay_vals[@]}"; do
@@ -38,13 +38,23 @@ for delay_val in "${delay_vals[@]}"; do
 	done
 done
 
-# primary experiments
+# Bandwidth primary experiments
 echo "Bandwidth Primary experiments"
 bandwidth_vals=("75Kbps" "100Kbps" "125Kbps" "150Kbps" "175Kbps" "200Kbps" "225Kbps" "250Kbps" "275Kbps" "300Kbps")
 for bandwidth_val in "${bandwidth_vals[@]}"; do
 	echo "Bandwidth Primary value: $bandwidth_vals"
 	for policy_number in 20 40 60 80 99; do
 		run_experiment "bandwidth_primary" "$bandwidth_val" "$policy_number" &
+	done
+done
+
+# Bandwidth alternate experiments
+echo "Bandwidth alternate experiments"
+bandwidth_vals=("75Kbps" "100Kbps" "125Kbps" "150Kbps" "175Kbps" "200Kbps" "225Kbps" "250Kbps" "275Kbps" "300Kbps")
+for bandwidth_val in "${bandwidth_vals[@]}"; do
+	echo "Bandwidth Alternate value: $bandwidth_vals"
+	for policy_number in 20 40 60 80 99; do
+		run_experiment "bandwidth_alternate" "$bandwidth_val" "$policy_number" &
 	done
 done
 
